992b936b4ff969fd395158cd0f09e914de8c6930
ThNave <evanfornarelli@gmail.com>
2021-11-16 18:55:48 +0100

    first commit

---
+++ src/app/components/table-multiplication/table-multiplication.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-multiplication',
+  templateUrl: './table-multiplication.component.html',
+  styleUrls: ['./table-multiplication.component.css']
+})
+export class TableMultiplicationComponent implements OnInit {
+
+
+  
+  
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  
+}
+
